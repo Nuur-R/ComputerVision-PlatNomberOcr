4adb4b9dff5adfb5d068af7fc4d1115e7710d8d5
Nuur-R <firdausnuurr@gmail.com>
2023-08-22 17:50:08 +0700

    Send Massage to Telegram

--- main.py
+++ main.py
@@ -1,16 +1,74 @@
-from ocrt import perform_ocrt
-from platDetection import plateDetec
-import multiprocessing
+import cv2
+import pytesseract
+import csv
+from datetime import datetime
+from tele import sendToTelegram
+class PlateDetector:
+    def __init__(self, cascade_path):
+        self.plate_cascade = cv2.CascadeClassifier(cascade_path)
+    
+    def detect_plates(self, frame):
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        plates = self.plate_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(25, 25))
+        return plates
+    
+    def draw_plates(self, frame, plates):
+        for (x, y, w, h) in plates:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-# membaca text dari dataaa.csv
-with open('data.csv', 'r') as f:
-    data = f.read()
+def recognize_text(image):
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    detected_text = pytesseract.image_to_string(gray_image, lang='eng')
+    return detected_text
 
-# multiprocessing untuk menjalaanan dua fungsi sekaligus dari fungsi perform_ocr dan plateDetec
-if __name__ == '__main__':
-    p1 = multiprocessing.Process(target=perform_ocrt, args=('plate/gambarPlate.jpg',))
-    p2 = multiprocessing.Process(target=plateDetec, args=('-',))
-    p1.start()
-    p2.start()
-    p1.join()
-    p2.join()
+def save_to_csv(data):
+    with open('data.csv', mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(data)
+        
+# Buka video dari webcam
+cap = cv2.VideoCapture(0)
+
+# Inisialisasi kelas
+plate_detector = PlateDetector('haarcascade_russian_plate_number_mod.xml')
+
+while True:
+    ret, frame = cap.read()  # Baca frame dari webcam
+    if not ret:
+        break
+    
+    plates = plate_detector.detect_plates(frame)
+    plate_detector.draw_plates(frame, plates)
+    
+    for (x, y, w, h) in plates:
+        plate_img = frame[y:y+h, x:x+w]
+        detected_text = recognize_text(plate_img)
+        
+        if detected_text.strip():
+            cv2.putText(frame, detected_text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+            save_to_csv([datetime.now().strftime('%Y-%m-%d %H:%M:%S'),detected_text])
+            image_path = str('plate/plate-{}-{}.jpg'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), detected_text))
+            cv2.imwrite(image_path, plate_img)
+            # convert detected_text to string
+            detected_text = str(detected_text)
+            sendToTelegram(detected_text, image_path)
+            
+        else:
+            cv2.putText(frame, 'Undetected', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        
+        cv2.imshow('Plate', plate_img)
+
+        # jika ada input S maka akan menyimpan gambar
+        if cv2.waitKey(1) & 0xFF == ord(' '):
+            save_to_csv([datetime.now().strftime('%Y-%m-%d %H:%M:%S'),detected_text])
+            # simpan gambar dengan format plate-time-detectedtext.jpg
+            cv2.imwrite('plate/plate-{}-{}.jpg'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), detected_text), plate_img)
+    
+    cv2.imshow('Plate Detection', frame)
+    
+    # Tekan tombol 'q' untuk keluar
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ tele.py
@@ -0,0 +1,29 @@
+API_KEY = '6453080762:AAFk3CWyEoXAcckSDqk-tx4bGMt-lCewhyQ'
+CHAT_ID = 5119690404
+
+import asyncio
+from telegram import Bot
+from telegram import InputFile
+
+# Ganti dengan token bot Telegram Anda
+BOT_TOKEN = API_KEY
+
+async def send_text_message(chat_id, text):
+    bot = Bot(token=BOT_TOKEN)
+    await bot.send_message(chat_id=chat_id, text=text)
+
+async def send_photo_to_user(token, chat_id, photo_path, caption=None):
+    bot = Bot(token=token)
+    await bot.send_photo(chat_id=chat_id, photo=open(photo_path, 'rb'), caption=caption)
+
+
+
+def sendToTelegram(text, photo_path):
+    try:
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(send_photo_to_user(API_KEY, CHAT_ID, photo_path, text))
+        print('Pesan foto berhasil dikirim ke obrolan dengan ID:', CHAT_ID)
+        
+    except Exception as e:
+        print("Terjadi kesalahan:", e)
+# sendToTelegram('test', 'test_plate/A.jpg')
